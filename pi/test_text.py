c6fd9923edd5175d1e20ee8121055ab2fa588f60
topherCantrell <topherCantrell@gmail.com>
2016-03-12 04:08:00 -0600

    Text

---
+++ pi/test_text.py
@@ -0,0 +1,18 @@
+from OLED import OLED
+from OLEDWindow import OLEDWindow
+
+# The OLED hardware driver
+oled = OLED()
+
+window_one = OLEDWindow(oled,0,0,256,64)
+
+for y in xrange(4):
+    mes = ""
+    for x in xrange(32):
+        mes = mes + chr(y*32+x) 
+    window_one.draw_text(0,y*8,mes,15)
+    
+window_one.draw_big_text(0,48,"Big and dim !",2)
+
+window_one.draw_screen_buffer()
+
